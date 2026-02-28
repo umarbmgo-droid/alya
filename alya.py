19146fdfa771017bcf6cf95a7d2eb5f636a952e4
umarbmgo-droid <umarbmgo@gmail.com>
2026-02-28 06:11:48 +0500

    Update alya.py

--- alya.py
+++ alya.py
@@ -6,7 +6,7 @@ import asyncio
 import time
 
 # ===== CONFIG =====
-TOKEN = "MTQ3NzA5MzcxMjE2NDE2MzU5NQ.G3Mygs.QkCulO8bNWWyykMF7c7r288rgItzJc6RfmIFGU"
+TOKEN = os.environ.get('TOKEN')
 OWNER_ID = 361069640962801664
 PREFIX = "-"
 START_TIME = time.time()
@@ -202,3 +202,4 @@ if __name__ == "__main__":
     except Exception as e:
 
         print(f"Error: {e}")
+
